3eb75038f3e73cf9c4821faea038b3548b8f651f
sarojsekhar <sarojsekhar25@gmail.com>
2025-08-30 16:13:13 +0530

    - proj-auth project created, backend server mongodb connection and   register api is created.

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection area start....
+    const uri = "mongodb://localhost:27017/";
+    const client = new MongoClient(uri);
+    let usersCollection;
+
+    async function connectDB() {
+        await client.connect();
+        const db = client.db("authDB");
+        usersCollection = db.collection("users");
+        console.log("MongoDB connected!....");
+    }
+
+    connectDB();
+// MongoDB Connection area end....
+
+// Register API endpoint start
+    app.post("/api/register", async (req, res) => {
+        try{
+            const { username, email, password } = req.body;
+            const existingUser = usersCollection.findOne({ email });
+            if ( existingUser ) {
+                return res.status(400).json({ message: "User already exists" });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            await usersCollection.insertOne({
+                username,
+                email,
+                password: hashedPassword,
+                createdAt: new Date()
+            });
+
+            res.status(201).json({ message: "User registered successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Error registering user", error });
+        }
+    });
+// Register API endpoint end
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
